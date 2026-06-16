d38d0bbee9fe811742057fbdb3ae7c3afa5e846e
Nitrajsinh-Solanki <nrsolanki2005@gmail.com>
2026-06-16 23:35:07 +0530

    some problem solved starting the dsa in java

---
+++ 01_basics/01_LengthOfDigit.java
@@ -0,0 +1,19 @@
+//  Count the total number of digits present in a given integer (n).
+
+import java.util.Scanner ; 
+
+public class CountDigits{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number ");
+        int n = sc.nextInt();
+        int count = 0 ; 
+        while(n >  0 ){
+            n = n / 10 ; 
+            count ++ ; 
+        }
+        System.out.println("total digits in number is "+ count);
+        
+    }
+}
---
+++ 01_basics/02_SumOfDigits.java
@@ -0,0 +1,23 @@
+// Calculate the sum of all individual digits in a given integer (n).
+
+import java.util.Scanner ; 
+
+public class SumOfDigits{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number ");
+        int n = sc.nextInt();
+        int sum = 0 ; 
+
+        while(n > 0 ){
+            int last_digit  = n % 10 ; 
+            sum += last_digit;
+            n = n / 10 ; 
+
+        }
+        System.out.println("sum of digit is "+ sum);
+
+        
+    }
+}
